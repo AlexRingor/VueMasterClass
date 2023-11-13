556e36f1175bf33e1345ec353c4611476e62a0f1
Alex Ringor <alexanderringor@gmail.com>
2023-11-13 15:28:42 +0800

    options API for displaying list of elements

---
+++ vue-forum/src/components/PageHome.vue
@@ -0,0 +1,38 @@
+<template>
+	<div>
+		<p>Hello from pageHome</p>
+		<div v-for="thread in threads" :key="thread.id">
+			<h1>{{thread.title}}</h1>
+      <div v-for="postId in thread.posts" :key="postId">
+        <p>{{userById(postById(postId).userId).name}}</p>
+        <p>{{postById(postId).text}}</p>
+      </div>
+		</div>
+	</div>
+</template>
+
+<script>
+import sourceData from '@/data.json'
+export default {
+  data () {
+    return {
+      threads: sourceData.threads,
+      posts: sourceData.posts,
+      users: sourceData.users
+    }
+  },
+  methods: {
+    postById (postId) {
+      return this.posts.find(p => p.id === postId)
+    },
+    userById (postId) {
+      return this.users.find(u => u.id === postId)
+    }
+  }
+}
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ vue-forum/src/main.js
@@ -0,0 +1,7 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+const forumApp = createApp(App)
+
+forumApp
+  .mount('#app')
