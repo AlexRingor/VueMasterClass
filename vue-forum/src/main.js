37166c7bb6b0958072965c6395d45568abbac0c1
Alex Ringor <alexanderringor@gmail.com>
2023-11-23 21:47:25 +0800

    separate routes to a different file for as best practice

--- vue-forum/src/main.js
+++ vue-forum/src/main.js
@@ -1,28 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import PageHome from '@/components/PageHome'
-import PageThreadShow from '@/components/PageThreadShow'
-import { createRouter, createWebHistory } from 'vue-router'
-
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: PageHome
-  },
-  {
-    path: '/thread/:id',
-    name: 'ThreadShow',
-    component: PageThreadShow,
-    props: true
-  }
-]
-
-const router = createRouter({
-  // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
-  history: createWebHistory(),
-  routes // short for `routes: routes`
-})
+import router from '@/router/index.js'
 
 const forumApp = createApp(App)
 
